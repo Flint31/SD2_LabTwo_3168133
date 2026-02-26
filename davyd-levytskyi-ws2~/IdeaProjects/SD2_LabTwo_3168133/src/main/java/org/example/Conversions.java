5bc1eedceabd08cf1b68535ec651e502818962a4
Davyd <levidskijdavid@gmail.com>
2026-02-26 00:28:08 +0000

    Add initial project files

---
+++ davyd-levytskyi-ws2~/IdeaProjects/SD2_LabTwo_3168133/src/main/java/org/example/Conversions.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/**
+ * Name: Давид Левицкий
+ * Student Number: 3168133
+ */
+public class Conversions {
+
+    // Converts euro to dollar
+    public double euroToDollar(double euro) {
+        return 0;
+    }
+
+    // Converts dollar to euro
+    public double dollarToEuro(double dollar) {
+        return 0;
+    }
+
+    // Converts a String to an int
+    public int stringToInteger(String val) {
+        return 0;
+    }
+
+    // Converts an int to a String
+    public String integerToString(int val) {
+        return null;
+    }
+
+    // Change uppercase to lowercase and vice versa
+    public String switchCase() {
+        return null;
+    }
+}
