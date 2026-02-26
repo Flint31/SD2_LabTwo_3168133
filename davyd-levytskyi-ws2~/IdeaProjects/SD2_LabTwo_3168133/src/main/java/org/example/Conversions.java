e3d002dbe4d086ef2b5eadc9290fb0a52563400c
Davyd <levidskijdavid@gmail.com>
2026-02-26 00:41:03 +0000

    test: add euroToDollar tests (failing)

--- davyd-levytskyi-ws2~/IdeaProjects/SD2_LabTwo_3168133/src/main/java/org/example/Conversions.java
+++ davyd-levytskyi-ws2~/IdeaProjects/SD2_LabTwo_3168133/src/main/java/org/example/Conversions.java
@@ -1,32 +1,35 @@
-package org.example;
-
 /**
- * Name: Давид Левицкий
+ * Name: Davyd Levytskyi
  * Student Number: 3168133
  */
+package org.example;
+
+
 public class Conversions {
 
-    // Converts euro to dollar
+     // Converts euro to dollar
     public double euroToDollar(double euro) {
         return 0;
     }
 
+
+
     // Converts dollar to euro
-    public double dollarToEuro(double dollar) {
+    public double dollarToEuro(double  dollar) {
         return 0;
     }
 
     // Converts a String to an int
-    public int stringToInteger(String val) {
+    public int  stringToInteger(String val) {
         return 0;
     }
 
     // Converts an int to a String
-    public String integerToString(int val) {
+    public String integerToString(int val)  {
         return null;
     }
 
-    // Change uppercase to lowercase and vice versa
+    // Change  uppercase to lowercase and vice versa
     public String switchCase() {
         return null;
     }
---
+++ davyd-levytskyi-ws2~/IdeaProjects/SD2_LabTwo_3168133/src/test/java/org/example/ConversionsTest.java
@@ -0,0 +1,21 @@
+/**
+ * Name: Davyd Levytskyi
+ * Student Number: 3168133
+ */
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ConversionsTest {
+
+    private final Conversions conversions = new Conversions();
+
+    @Test
+    void testEuroToDollar() {
+        assertEquals(0.0, conversions.euroToDollar(0.0), 0.0001);
+        assertEquals(110.0, conversions.euroToDollar(100.0), 0.0001);
+        assertEquals(-11.0, conversions.euroToDollar(-10.0), 0.0001);
+    }
+}
